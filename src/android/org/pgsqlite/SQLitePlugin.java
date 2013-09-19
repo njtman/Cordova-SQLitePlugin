75d25df7892724a35dfcab25bdf82d5709f5ebea
njtman <tancer111@gmail.com>
2013-09-19 12:04:50 -0400

    Update SQLitePlugin.java

--- src/android/org/pgsqlite/SQLitePlugin.java
+++ src/android/org/pgsqlite/SQLitePlugin.java
@@ -16,8 +16,8 @@ import java.lang.Number;
 
 import java.util.HashMap;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.CallbackContext;
 
 import android.database.Cursor;
 
